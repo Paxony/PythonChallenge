a391531a14b3538e01e83cef8280cc9bf460c3d7
Paola Correa Manterola <paola.correa@alen.com.mx>
2019-09-17 20:25:04 -0500

    ok

--- PyPoll/Main.py
+++ PyPoll/Main.py
@@ -1,45 +1,65 @@
-#Importing my path
-import os
+#setting my environment
 import csv
+import os
 
-#define my path to the file
-filecsv=os.path.join("..","PythonChallenge","PyPoll","Election_data.csv")
+#defining the path
+PyPollcsv =os.path.join("..","PythonChallenge","PyPoll","Election_data.csv")
 
-#setting the variables
-votes = 0
-candidatelist = []
+"setting my variable"
+count = 0
 candidatename = []
+candidate = []
 vote_count = []
 vote_percent = []
 
-# Open the CSV using the set path PyPollcsv
+# giving access to my file
 
-with open(filecsv, newline="") as csvfile:
+with open(PyPollcsv, newline="") as csvfile:
     csvreader = csv.reader(csvfile, delimiter=",")
     csv_header = next(csvreader)
-    # define the information
+    # reading the information by row
     for row in csvreader:
         # Count the total number of votes
-        votes = votes + 1
-        # Set the candidate names to list
-        candidatelist.append(row[2])
-        # Create a list the names
-    for x in list(candidatelist):
-        candidatename.append(x)
+        count = count + 1
+        # Set the candidate names to candidatename
+        candidatename.append(row[2])
+        # Create a set from the candidatename to get the unique candidate names
+    for x in set(candidatename):
+        candidate.append(x)
         # y is the total number of votes per candidate
-        y = candidatelist.count(x)
+        y = candidatename.count(x)
         vote_count.append(y)
         # z is the percent of total votes per candidate
-        z = (y/votes)*100
+        z = (y/count)*100
         vote_percent.append(z)
         
     winning_vote_count = max(vote_count)
+    winner = candidate[vote_count.index(winning_vote_count)]
     
-# output to a text file
-#file = open("PollResults.txt","w")
-#file.write("Election Results" + "\n")
-#file.write("...................................................................................." + "\n")
-#file.write(f"Total votes:(Votes)"+ "\n")
-#file.write("...................................................................................." + "\n")
-#file.write()
-#file.close()
+
+# Printing the results
+print("-------------------------")
+print("Election Results")   
+print("-------------------------")
+print("Total Votes :" + str(count))    
+print("-------------------------")
+for i in range(len(candidate)):
+            print(candidate[i] + ": " + str(round(vote_percent[i])) +"% (" + str(vote_count[i])+ ")")
+print("-------------------------")
+print("The winner is: " + winner)
+print("-------------------------")
+
+#sending to a file 
+with open("PollResults.txt", "w") as file:
+    file.write("-------------------------")
+    file.write("Election Results")   
+    file.write("-------------------------")
+    file.write("Total Votes :" + str(count))    
+    file.write("-------------------------")
+    for i in range(len(candidate)):
+            file.write(candidate[i] + ": " + str(round(vote_percent[i])) +"% (" + str(vote_count[i])+ ")")
+    file.write("-------------------------")
+    file.write("The winner is: " + winner)
+    file.write("-------------------------")
+    
+
